cc82aaa23304ccb4700271c3fd1eb1412b14eb71
Michał Szafrański <szafranski@gmail.com>
2020-08-28 14:23:09 +0000

    support analog classic controller

--- i2c-classic-controller.c
+++ i2c-classic-controller.c
@@ -11,6 +11,10 @@
 
 #define GET(r, i) ((r & (1 << i)) == 0)
 
+int hq = 0;
+int analog = 0;
+int debug = 0;
+
 int classic_button_map[16] = {
   0,
   BTN_TR,
@@ -29,7 +33,11 @@ int classic_button_map[16] = {
   BTN_SOUTH,
   BTN_TR2,
 };
-
+int classic_axis_map[6] = {
+  ABS_X, ABS_RX,
+  ABS_Y, ABS_RY,
+  ABS_Z, ABS_RZ,
+};
 static void setup_abs(int fd, unsigned chan, int min, int max) {
   struct uinput_abs_setup s = {
     .code = chan,
@@ -46,13 +54,16 @@ int uinput_init() {
   for (int i = 0; i < 16; i++) {
     classic_button_map[i] && ioctl(fd, UI_SET_KEYBIT, classic_button_map[i]);
   }
-  /* Analog input not supported yet, but adding the basic axes is required
-     for some software to recognize the controller as a gamepad. */
+  /* Adding the basic axes is required for some software to recognize the controller as a gamepad. */
   ioctl(fd, UI_SET_EVBIT, EV_ABS);
   setup_abs(fd, ABS_X,  -127, 127);
   setup_abs(fd, ABS_Y,  -127, 127);
-  setup_abs(fd, ABS_Z,  -127, 127);
-  setup_abs(fd, ABS_RZ,  -127, 127);
+  setup_abs(fd, ABS_RX,  -127, 127);
+  setup_abs(fd, ABS_RY,  -127, 127);
+  if (analog == 6) {
+    setup_abs(fd, ABS_Z,  -127, 127);
+    setup_abs(fd, ABS_RZ,  -127, 127);
+  }
   memset(&usetup, 0, sizeof(usetup));
   usetup.id.bustype = BUS_USB;
   usetup.id.vendor = 0x1209; // Generic
@@ -72,7 +83,7 @@ void uinput_emit(int fd, int type, int code, int val) {
   write(fd, &ie, sizeof(ie));
 }
 
-void emit_events(int fd, int r, int r_prev) {
+void emit_events(int fd, unsigned short r, unsigned short r_prev, unsigned char *a, unsigned char *a_prev) {
   int syn = 0;
   for (int i = 0; i < 16; i++) {
     int btn = classic_button_map[i];
@@ -81,9 +92,52 @@ void emit_events(int fd, int r, int r_prev) {
       syn = 1;
     }
   }
+  for (int i = 0; i < analog; i++) if (a[i] != a_prev[i]) {
+    uinput_emit(fd, EV_ABS, classic_axis_map[i], a[i] - 128);
+    syn = 1;
+  }
   if (syn) {
+    if (debug) {
+      if (analog) printf("axes x:%4d y:%4d rx:%4d ry:%4d ", a[0] - 128, a[2] - 128, a[1] - 128, a[3] - 128);
+      if (analog == 6) printf("z:%02X rz:%02X ", a[4] - 128, a[5] - 128);
+      printf("butons %04X\r", r ^ 0xffff);
+      fflush(stdout);
+    }
     uinput_emit(fd, EV_SYN, SYN_REPORT, 0);
   }
+}
+#define LX(a) ((a)->lx << 2)
+#define LY(a) ((a)->ly << 2)
+#define RX(a) (((a)->rx2 + ((a)->rx1 << 1) + ((a)->rx0 << 3)) << 3)
+#define RY(a) ((a)->ry << 3)
+#define LT(a) (((a)->lt1 + ((a)->lt0 << 3)) << 3)
+#define RT(a) ((a)->rt << 3)
+void to_hq(unsigned char from[4], unsigned char to[6]) {
+  struct {
+    unsigned char lx:6, rx0:2;
+    unsigned char ly:6, rx1:2;
+    unsigned char ry:5, lt0:2;
+    unsigned char rx2:1, rt:5, lt1:3;
+    unsigned short buttons;
+  } *a = (void*)from;
+  to[0] = a->lx << 2;
+  to[1] = (a->rx2 + (a->rx1 << 1) + (a->rx0 << 3)) << 3;
+  to[2] = a->ly << 2;
+  to[3] = a->ry << 3;
+  to[4] = (a->lt1 + (a->lt0 << 3)) << 3;
+  to[5] = a->rt << 3;
+}
+void emit_analog(int fd, char *pr, char *pr_prev) {
+  unsigned char hq_axes[6], hq_axes_prev[6];
+  to_hq(pr, hq_axes);
+  to_hq(pr_prev, hq_axes_prev);
+  emit_events(fd, *(unsigned short *)(pr + 4), *(unsigned short *)(pr_prev + 4), hq_axes, hq_axes_prev);
+}
+void emit_analog_hq(int fd, char *pr, char *pr_prev) {
+  emit_events(fd, *(unsigned short *)(pr + 6), *(unsigned short *)(pr_prev + 6), pr, pr_prev);
+}
+void emit_digital(int fd, char *pr, char *pr_prev) {
+  emit_events(fd, *(unsigned short *)pr, *(unsigned short *)pr_prev, 0, 0);
 }
 
 #define RECONNECT_DELAY_US 500000
@@ -91,10 +145,11 @@ void emit_events(int fd, int r, int r_prev) {
 #define RETRIES_MAX 5
 #define I2C_DELAY_US 1
 
+int long_wait = 0;
 int read_bytes(int file, int offset, int c, char *to) {
   int res = i2c_smbus_write_byte(file, offset);
   if (res < 0) return res;
-  usleep(I2C_DELAY_US);
+  usleep(offset ? I2C_DELAY_US : 120);
   for (int i = 0; i < c; i++) {
     res = i2c_smbus_read_byte(file);
     if (res < 0) return res;
@@ -110,6 +165,9 @@ int initialize(int file) {
   res = i2c_smbus_write_byte_data(file, 0xfb, 0x00);
   if (res < 0) return res;
   usleep(I2C_DELAY_US);
+  res = i2c_smbus_write_byte_data(file, 0xfe, hq ? 0x03 : 0x01);
+  if (res < 0) return res;
+  usleep(I2C_DELAY_US);
   return 0;
 }
 
@@ -118,7 +176,7 @@ int main(int argc, char **argv) {
   int hz = 60;
   char filename[20];
   int opt;
-  while ((opt = getopt(argc, argv, "y:f:")) != -1) {
+  while ((opt = getopt(argc, argv, "y:f:ha::d")) != -1) {
     switch (opt) {
     case 'y':
         adapter_nr = atoi(optarg);
@@ -128,11 +186,24 @@ int main(int argc, char **argv) {
         if (hz < 1) hz = 1;
         if (hz > 1000) hz = 1000;
         break;
+    case 'h':
+        hq = 1;
+        break;
+    case 'a':
+        analog = optarg ? atoi(optarg) : 4;
+        if (analog != 0 && analog != 4 && analog != 6) analog = 0;
+        break;
+    case 'd':
+        debug = 1;
+        break;
     default:
-        fprintf(stderr, "Usage: %s [-f <scan freq=60>] [-y <bus index=1>]\n", argv[0]);
+        fprintf(stderr, "Usage: %s [-d] [-f <scan freq=60>] [-y <bus index=1>] [-h] [-a|-a6]\n", argv[0]);
         exit(EXIT_FAILURE);
     }
   }
+  int read_len = analog ? hq ? 8 : 6 : 2;
+  int read_addr = analog ? 0 : hq ? 6 : 4;
+  void (*parse_func)(int fd, char *pr, char *pr_prev) = analog ? hq ? emit_analog_hq : emit_analog : emit_digital;
 
   snprintf(filename, sizeof(filename) - 1, "/dev/i2c-%d", adapter_nr);
   int file = open(filename, O_RDWR);
@@ -149,16 +220,16 @@ int main(int argc, char **argv) {
     perror("ioctl");
     exit(1);
   }
-  unsigned short r_prev = 0;
+  unsigned long long r_prev;
   int connected = 0;
   int hearbeat = 0;
   for(;;) {
     if (connected) {
-      unsigned short r;
-      int res = read_bytes(file, 0x04, 2, (void*)&r);
+      unsigned long long r;
+      int res = read_bytes(file, read_addr, read_len, (void *)&r);
       for(int retries = 0; res < 0 && retries < RETRIES_MAX; retries++) {
         usleep(RETRY_DELAY_US);
-        res = read_bytes(file, 0x04, 2, (void*)&r);
+        res = read_bytes(file, read_addr, read_len, (void *)&r);
       }
       if (res < 0) {
         connected = 0;
@@ -166,11 +237,7 @@ int main(int argc, char **argv) {
         perror("read");
         continue;
       }
-      if (r != r_prev) {
-        printf("rd %04X\r", r ^ 0xffff);
-        fflush(stdout);
-      }
-      emit_events(uinput_fd, r, r_prev);
+      parse_func(uinput_fd, (void*)&r, (void *)&r_prev);
       r_prev = r;
       if (++hearbeat % (hz * 2) == 0) {
         unsigned char id;
